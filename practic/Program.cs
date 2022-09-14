52026df93f5e5e89118b9b59c853ed783d4a0341
Sergey <tr4k@mail.ru>
2022-09-14 21:25:41 +0300

    add AddArray function

--- practic/Program.cs
+++ practic/Program.cs
@@ -1,16 +1,29 @@
-﻿void Write(string[] a)
+﻿
+static void Print(string[] a)
 {
+    Console.Write("[");
     for (int i = 0; i < a.Length; i++)
     {
-        Console.Write(a + " ");
+        if (a[i].Length > 0)
+        {
+            Console.Write($"'{a[i]}',");
+        }
     }
-
+    Console.WriteLine("]");
 }
-static string[] Array(string[] a)
+static string[] AddArray(string[] a)
 {
-    
-    return a;
+    string[] res = new string[a.Length];
+    for (int i = 0; i < a.Length; i++)
+    {
+        if (a[i].Length <= 3)
+        {
+            res[i] = a[i];
+        }
+    }
+    return res;
 }
-string[] a = {"hello", "2", ":-)"};
-
-Write(a);
+string[] a = { "hello", "2", ":-)" };
+string[] b = AddArray(a);
+b = b.Where(x => x != null).ToArray();
+Print(b);
