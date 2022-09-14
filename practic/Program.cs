fa8793f67595eac216623c790bc1bfea00497c9f
Sergey <tr4k@mail.ru>
2022-09-14 15:05:18 +0300

    add write function

--- practic/Program.cs
+++ practic/Program.cs
@@ -1,1 +1,16 @@
-﻿
+﻿void Write(string[] a)
+{
+    for (int i = 0; i < a.Length; i++)
+    {
+        Console.Write(a + " ");
+    }
+
+}
+static string[] Array(string[] a)
+{
+    
+    return a;
+}
+string[] a = {"hello", "2", ":-)"};
+
+Write(a);
